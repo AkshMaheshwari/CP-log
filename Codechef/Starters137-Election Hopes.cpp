0c8b71f97ceb3c5db719503342c2c4ce639904ce
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-14 10:55:35 +0530

    Create Starters137-Election Hopes.cpp

---
+++ Codechef/Starters137-Election Hopes.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    int a,b;
+    cin >> a >> b;
+    if(a >= 2*b)cout << "yes"<<endl;
+    
+    else cout << "No"<<endl;
+    return 0;
+}
