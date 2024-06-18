b4040345847aa54a8155888ce77b1f6bdd5178aa
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-18 09:54:00 +0530

    Update 1306-Smallest Possible whole number.cpp

--- Codechef/1000+/1306-Smallest Possible whole number.cpp
+++ Codechef/1000+/1306-Smallest Possible whole number.cpp
@@ -7,13 +7,13 @@ int32_t main(){
     cin >> t;
     while(t--)
     {
-        int a,b,a1,b1,a2,b2;
-        cin >> a >> b >> a1 >> b1 >> a2 >> b2;
-        if((a == a1 || a == b1)&& (b == a1 || b == b1))cout << 1 << endl;
+        int a,b;
+        cin >> a >> b;
         
-        else if((a == a2 || a == b2) && (b == a2 || b == b2))cout << 2 << endl;
+        if(b == 0)cout << a << endl;
         
-        else cout << 0 << endl;
+        else cout << a % b << endl;
+        
     }
     return 0;
 }
