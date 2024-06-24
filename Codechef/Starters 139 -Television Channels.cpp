0daea0b0bff96e24a9fac5679cc67c2a1b622f52
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-24 11:40:59 +0530

    Create Starters 139 -Television Channels.cpp

---
+++ Codechef/Starters 139 -Television Channels.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    int x;
+    cin >> x;
+    if(x % 2 == 0)cout << x/2 <<endl;
+    
+    else cout << 1+x/2<<endl;
+    return 0;
+}
