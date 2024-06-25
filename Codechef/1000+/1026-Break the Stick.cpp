dd59d4eba30ddb3e79430aa26fe1d2b050c9bdac
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-25 11:10:19 +0530

    Create 1026-Break the Stick.cpp

---
+++ Codechef/1000+/1026-Break the Stick.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+        cin >>a >>b;
+        if(a % 2!=0 && b % 2 == 0)cout << "NO"<<endl;
+        
+        else cout << "Yes"<<endl;
+      
+      
+    
+ 
+    }
+    return 0;
+}
