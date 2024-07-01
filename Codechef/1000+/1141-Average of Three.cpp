74776d5f4333948e087a4e5e655885abb0cadc81
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-07-01 13:12:50 +0530

    Create 1141-Average of Three.cpp

---
+++ Codechef/1000+/1141-Average of Three.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int mean;
+        cin >> mean;
+        cout << mean-1 <<" "<<mean<<" "<<mean+1<<endl;
+    }
+    return 0;
+}
