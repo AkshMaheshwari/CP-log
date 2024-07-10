480751154eac7bcb3b53dd76d21ec0b519be2f4e
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-07-10 12:45:18 +0530

    Create 1098-Friends Meetup.cpp

---
+++ Codechef/1000+/1098-Friends Meetup.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x,y;
+        cin >> x >> y;
+        if(y > x)cout << "NO"<<endl;
+        
+        else cout << "YES"<<endl;
+    }
+    return 0;
+}
