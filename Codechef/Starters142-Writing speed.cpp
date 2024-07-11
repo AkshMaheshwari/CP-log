1e5f1a7af5e238777e601a15eb5b0fd86f565ab1
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-07-11 15:20:06 +0530

    Create Starters142-Writing speed.cpp

---
+++ Codechef/Starters142-Writing speed.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main(){
+    int x;
+    cin >> x;
+    if(x > 12)cout << "No"<<endl;
+    
+    else cout << "Yes"<<endl;
+    return 0;
+}
